c67e63a398e975965c06608b634b273b933b401f
shadowiq <OBRIENWT5773@uwec.edu>
2018-02-14 14:19:06 -0600

    Deleted all files. Needed to fix issue / have new branch for Unity

--- Assets/Enemy.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
- using UnityEngine;
-
-public abstract class Enemy : IEntity {
-
-    public Enemy(int startingHP, int cardsPerTurn, int energyPerTurn, List<Card> cards) : base(startingHP, cardsPerTurn, energyPerTurn, cards) {
-    }
-}
